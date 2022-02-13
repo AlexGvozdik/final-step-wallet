4ac3ece76fd0da1726b0e96a41c9d44ed3e6d773
yeliseyeva <nik29121997@gmail.com>
2022-02-13 20:43:54 +0200

    Doughnut

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+
 const App = () => {
-    return (
-        <>
-            <div>Hello Blended_3!!!</div>
-        </>
-    );
+  return (
+    <>
+      <div>Hello Blended_3!!!</div>
+    </>
+  );
 };
 
 export default App;
