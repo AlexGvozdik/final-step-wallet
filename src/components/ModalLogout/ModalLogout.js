288620e8545d05214c9d0d650bbff6b5a66dd79f
yeliseyeva <nik29121997@gmail.com>
2022-02-15 18:33:46 +0200

    modal is done

--- src/components/ModalLogout/ModalLogout.js
+++ src/components/ModalLogout/ModalLogout.js
@@ -1,27 +1,24 @@
+import Backdrop from "@mui/material/Backdrop";
 import s from "./ModalLogout.module.css";
+import iconClose from "../../images/close.svg";
 
 function ModalLogout() {
   return (
-    <div className={s.modalLogoutConteiner}>
-      <h2 className={s.logoutHeader}>Выход из личного кабинета</h2>
-      <p className={s.logoutText}>Вы действительно хотите выйти?</p>
-      <div>
-        <button className={s.logoutButton}>Выйти</button>
-        <button className={s.cancelButton}>Отменить</button>
+    <Backdrop open="true">
+      <div className={s.modal}>
+        <div className={s.modalLogoutConteiner}>
+          <h2 className={s.logoutHeader}>Выход из личного кабинета</h2>
+          <p className={s.logoutText}>Вы действительно хотите выйти?</p>
+          <div>
+            <button className={s.logoutButton}>Выйти</button>
+            <button className={s.cancelButton}>Отменить</button>
+          </div>
+        </div>
+        <button className={s.buttonClose}>
+          <img src={iconClose} width="14px" height="14px" />
+        </button>
       </div>
-      <button className={s.buttonClose}>
-        <svg
-          width="18"
-          height="18"
-          viewBox="0 0 18 18"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M1 1L17 17" stroke="black" />
-          <path d="M1 17L17 0.999999" stroke="black" />
-        </svg>
-      </button>
-    </div>
+    </Backdrop>
   );
 }
 
