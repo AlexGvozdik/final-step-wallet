090483586bf8513746b5899f5adee3de88f95463
yeliseyeva <nik29121997@gmail.com>
2022-02-15 15:28:29 +0200

    modal logout

--- src/components/ModalLogout/ModalLogout.js
+++ src/components/ModalLogout/ModalLogout.js
@@ -1,15 +1,26 @@
 import s from "./ModalLogout.module.css";
-import iconClose from "../../images/exit-icon.svg";
 
 function ModalLogout() {
   return (
     <div className={s.modalLogoutConteiner}>
       <h2 className={s.logoutHeader}>Выход из личного кабинета</h2>
       <p className={s.logoutText}>Вы действительно хотите выйти?</p>
-      <div className={s.buttonGroup}>
+      <div>
         <button className={s.logoutButton}>Выйти</button>
         <button className={s.cancelButton}>Отменить</button>
       </div>
+      <button className={s.buttonClose}>
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1 1L17 17" stroke="black" />
+          <path d="M1 17L17 0.999999" stroke="black" />
+        </svg>
+      </button>
     </div>
   );
 }
