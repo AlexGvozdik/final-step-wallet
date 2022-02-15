db77b4476a8e5fe1928ce3c6d81baf2ba46c2090
yeliseyeva <nik29121997@gmail.com>
2022-02-15 18:34:06 +0200

    modal is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import ModalLogout from "./components/ModalLogout/ModalLogout";
 const App = () => {
   return (
     <>
       <div>Hello Blended_3!!!</div>
-      <ModalLogout />
     </>
   );
 };
