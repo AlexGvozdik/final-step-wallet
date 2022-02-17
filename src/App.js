b21b8b4ba1c267c76c100c6ea1c8e8c62bce907d
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2022-02-17 10:30:21 +0200

    added notfoundPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MainView from './views/MainView/MainView';
+import NotFoundView from './views/NotFoundView/NotFoundView';
 
 const App = () => {
   return (
     <>
-      <MainView/>
+      <MainView />
     </>
   );
 };
