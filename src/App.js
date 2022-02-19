7e7f71e84efd47b9786df0b2b918dea4e0b15c1a
yeliseyeva <nik29121997@gmail.com>
2022-02-19 21:34:18 +0200

    table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MainView from './views/MainView/MainView';
 import NotFoundView from './views/NotFoundView/NotFoundView';
-import { Routes, Route } from "react-router-dom";
-import LoginView from "./views/LoginView";
-import Header from "./components/HeaderNav/HeaderNav";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { authOperations } from "./redux/auth";
+import { Routes, Route } from 'react-router-dom';
+import LoginView from './views/LoginView';
+import Header from './components/HeaderNav/HeaderNav';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { authOperations } from './redux/auth';
+import DiagramTab from './components/DiagramTab/DiagramTab';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -18,12 +19,13 @@ const App = () => {
   return (
     <>
       <MainView />
-    <div>
-      <Routes>
-        <Route path="/" exact element={<Header />} />
-        <Route path="/login" element={<LoginView />} />
-      </Routes>
-    </div>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<Header />} />
+          <Route path="/login" element={<LoginView />} />
+        </Routes>
+      </div>
+      <DiagramTab />
     </>
   );
 };
